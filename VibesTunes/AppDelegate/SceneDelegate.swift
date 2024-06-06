cfa254084c1c7d198af9c7eacf3fd41ebe702294
Bruno Alves <alves.bruno945@gmail.com>
2024-06-06 09:22:51 -0300

    Setup SceneDelegate

--- VibesTunes/AppDelegate/SceneDelegate.swift
+++ VibesTunes/AppDelegate/SceneDelegate.swift
@@ -5,7 +5,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        self.window?.windowScene = windowScene
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
