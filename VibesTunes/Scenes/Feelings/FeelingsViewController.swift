3a72f861b12d19930ebfe6fa270ddb89783ca138
Bruno Alves <alves.bruno945@gmail.com>
2024-06-06 09:48:12 -0300

    setup `FeelingsViewController`

--- VibesTunes/Scenes/Feelings/FeelingsViewController.swift
+++ VibesTunes/Scenes/Feelings/FeelingsViewController.swift
@@ -2,7 +2,44 @@ import UIKit
 
 final class FeelingsViewController: UIViewController {
 
+    // MARK: - Private Properties
+
+    private let searchItemButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Search Item", for: .normal)
+        button.configuration = .tinted()
+        button.configuration?.baseBackgroundColor = .systemRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
+        setupViewHierarchy()
+        setupConstraints()
+        setupBackgroundColor()
+    }
+
+    private func setupViewHierarchy() {
+        view.addSubview(searchItemButton)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            searchItemButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchItemButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    private func setupBackgroundColor() {
+        view.backgroundColor = .systemBackground
     }
 }
