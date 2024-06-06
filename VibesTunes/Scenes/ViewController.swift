5b545856363a2e371b58f1aca28521b9fd8b64fa
Bruno Alves <alves.bruno945@gmail.com>
2024-06-06 09:23:29 -0300

    ViewController

--- VibesTunes/Scenes/ViewController.swift
+++ VibesTunes/Scenes/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  VibesTunes
-//
-//  Created by Bruno Silva on 06/06/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
