55f2017069b8e9e450b3d6424dbe0a93bc5dd400
Bruno Alves <alves.bruno945@gmail.com>
2024-06-06 12:23:56 -0300

    Add URLSession

--- VibesTunes/Scenes/Feelings/NetworkManager/NetworkManager.swift
+++ VibesTunes/Scenes/Feelings/NetworkManager/NetworkManager.swift
@@ -6,5 +6,33 @@ final class NetworkManager {}
 
 extension NetworkManager: NetworkManagerType {
 
-    func fetchData() {}
+    func fetchData() {
+
+        //        guard let url = URL(string: "https://itunes.apple.com/search?" + "media=ebook&term=\(terms)") else { return }
+
+        guard let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson") else { return }
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+
+            if let error = error {
+                print("error: \(String(describing: error))")
+                return
+            }
+
+            guard let response = response else {
+                print("something went wrong with response \(String(describing: response))")
+                return
+            }
+
+            guard let data = data else {
+                print("something went wrong with data \(String(describing: data))")
+                return
+            }
+
+            let decoded = String(data: data, encoding: .utf8)
+            print("Data: \(String(describing: decoded))")
+        }
+        .resume()
+    }
 }
