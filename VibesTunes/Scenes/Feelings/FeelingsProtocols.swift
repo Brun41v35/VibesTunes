6c562f84634e2236b400d74f8d50603ba8f62cda
Bruno Alves <alves.bruno945@gmail.com>
2024-06-06 10:42:42 -0300

    Add `NetworkManager` and protocol

---
+++ VibesTunes/Scenes/Feelings/FeelingsProtocols.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol NetworkManagerType {
+    func fetchData()
+}
---
+++ VibesTunes/Scenes/Feelings/NetworkManager/NetworkManager.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+final class NetworkManager {}
+
+// MARK: - Extension
+
+extension NetworkManager: NetworkManagerType {
+
+    func fetchData() {}
+}
