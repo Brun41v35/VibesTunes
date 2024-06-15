b7b536b12c42e81670352724639291f85e2d430f
Bruno Alves <alves.bruno945@gmail.com>
2024-06-15 17:07:09 -0300

    Add `previewURL` propertier at `SongViewCellViewModel` and add `playMusic` ViewController

--- VibesTunes/Scenes/Feelings/Adapter/FeelingsAdapter.swift
+++ VibesTunes/Scenes/Feelings/Adapter/FeelingsAdapter.swift
@@ -4,7 +4,9 @@ final class FeelingsAdapter: FeelingsAdapterType {
 
     func adapt(songs: Songs) -> SongsModel {
         let listSongs = songs.results.map { value in
-            return SongViewCellViewModel(nameArtist: value.artistName, nameSong: value.trackName)
+            return SongViewCellViewModel(nameArtist: value.artistName,
+                                         nameSong: value.trackName,
+                                         previewURL: value.previewURL)
         }
         return SongsModel(listSongs: listSongs)
     }
--- VibesTunes/Scenes/Songs/View/SongViewCellViewModel.swift
+++ VibesTunes/Scenes/Songs/View/SongViewCellViewModel.swift
@@ -3,4 +3,5 @@ import Foundation
 struct SongViewCellViewModel {
     let nameArtist: String
     let nameSong: String
+    let previewURL: String
 }
