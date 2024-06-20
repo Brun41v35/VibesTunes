0b4796ee5e892578c272277f701d6085222c4a82
Bruno Alves <alves.bruno945@gmail.com>
2024-06-20 20:02:44 -0300

    Remove Text

--- VibesTunes/Scenes/Feelings/View/FeelingsView.swift
+++ VibesTunes/Scenes/Feelings/View/FeelingsView.swift
@@ -3,10 +3,7 @@ import SwiftUI
 struct FeelingsView: View {
 
     var body: some View {
-        VStack(spacing: 50) {
-            Text("Como você está se sentindo ?")
-                .font(.system(size: 30, weight: .bold))
-                .multilineTextAlignment(.center)
+        VStack {
             FeelingsButtonsView()
         }
     }
