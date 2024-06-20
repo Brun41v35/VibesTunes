a0c3a4d23dfc80e8d409f04fcc08680c57365f11
Bruno Alves <alves.bruno945@gmail.com>
2024-06-20 19:14:58 -0300

    Add Text

--- VibesTunes/Scenes/Feelings/View/FeelingsView.swift
+++ VibesTunes/Scenes/Feelings/View/FeelingsView.swift
@@ -3,39 +3,11 @@ import SwiftUI
 struct FeelingsView: View {
 
     var body: some View {
-        VStack(spacing: 10) {
-            Button {
-                print("Button Happy")
-            } label: {
-                Text("Feliz")
-                    .frame(width: 280, height: 50)
-                    .background(Color.green)
-                    .tint(.white)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .clipShape(.buttonBorder)
-            }
-
-            Button {
-                print("Button Normal")
-            } label: {
-                Text("Normal")
-                    .frame(width: 280, height: 50)
-                    .background(Color.blue)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .tint(.white)
-                    .clipShape(.buttonBorder)
-            }
-
-            Button {
-                print("Button Sad")
-            } label: {
-                Text("Triste")
-                    .frame(width: 280, height: 50)
-                    .background(Color.red)
-                    .tint(.white)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .clipShape(.buttonBorder)
-            }
+        VStack(spacing: 50) {
+            Text("Como você está se sentindo ?")
+                .font(.system(size: 30, weight: .bold))
+                .multilineTextAlignment(.center)
+            FeelingsButtonsView()
         }
     }
 }
