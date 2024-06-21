d614eb14db6aa3326dc5db3acb8d35e74ecef74b
Bruno Alves <alves.bruno945@gmail.com>
2024-06-21 17:38:47 -0300

    Add `SongsModel` to be a mockData and populate list

--- VibesTunes/Scenes/Songs/Model/SongsModel.swift
+++ VibesTunes/Scenes/Songs/Model/SongsModel.swift
@@ -1,5 +1,22 @@
 import Foundation
 
 struct SongsModel {
-    let listSongs: [SongViewCellViewModel]
+    let nameArtist: String
+    let nameSong: String
+}
+
+struct SongsList {
+
+    static var mockData: [SongsModel] {
+        return [
+            SongsModel(nameArtist: "Slipknot",
+                       nameSong: "Before I Forget"),
+
+            SongsModel(nameArtist: "Iron Maiden",
+                       nameSong: "Rainmaker"),
+
+            SongsModel(nameArtist: "Ariana Grande",
+                       nameSong: "Problem")
+        ]
+    }
 }
