296e5b9ea38deaea808fea431b6cdfc9d31ab189
Bruno Alves <alves.bruno945@gmail.com>
2024-06-29 11:56:57 -0300

    Add button

--- VibesTunes/Scenes/Feelings/View/FeelingsButtonsView.swift
+++ VibesTunes/Scenes/Feelings/View/FeelingsButtonsView.swift
@@ -1,44 +1,21 @@
 import SwiftUI
 
 struct FeelingsButtonsView: View {
+
+    var title: String
+    var backgroundButtonColor: Color
+
     var body: some View {
-        VStack(spacing: 10) {
-            Button {
-                print("Button Happy")
-            } label: {
-                Text("Feliz")
-                    .frame(width: 280, height: 50)
-                    .background(Color.green)
-                    .tint(.white)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .clipShape(.buttonBorder)
-            }
-
-            Button {
-                print("Button Normal")
-            } label: {
-                Text("Normal")
-                    .frame(width: 280, height: 50)
-                    .background(Color.blue)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .tint(.white)
-                    .clipShape(.buttonBorder)
-            }
-
-            Button {
-                print("Button Sad")
-            } label: {
-                Text("Triste")
-                    .frame(width: 280, height: 50)
-                    .background(Color.red)
-                    .tint(.white)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .clipShape(.buttonBorder)
-            }
-        }
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundButtonColor)
+            .tint(.white)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .clipShape(.buttonBorder)
     }
 }
 
 #Preview {
-    FeelingsButtonsView()
+    FeelingsButtonsView(title: "Text",
+                        backgroundButtonColor: Color.gray)
 }
