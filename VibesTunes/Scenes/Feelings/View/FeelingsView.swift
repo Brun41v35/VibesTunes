6452250e10539ac322a2c82a121a8ee789026fe0
Bruno Alves <alves.bruno945@gmail.com>
2024-07-10 22:28:18 -0300

    Add gradient background

--- VibesTunes/Scenes/Feelings/View/FeelingsView.swift
+++ VibesTunes/Scenes/Feelings/View/FeelingsView.swift
@@ -1,32 +1,40 @@
 import SwiftUI
 
 struct FeelingsView: View {
-    
+
     var body: some View {
-        
-        HStack {
-            Button {
-                print("Go to happy list")
-            } label: {
-                FeelingsButtonsView(iconFeeling: "☺️",
-                                    title: "Happy",
-                                    backgroundButtonColor: Color.green)
-            }
-            
-            Button {
-                print("Go to normal list")
-            } label: {
-                FeelingsButtonsView(iconFeeling: "😐",
-                                    title: "Normal",
-                                    backgroundButtonColor: Color.green)
-            }
-            
-            Button {
-                print("Go to sad list")
-            } label: {
-                FeelingsButtonsView(iconFeeling: "😢",
-                                    title: "Triste",
-                                    backgroundButtonColor: Color.green)
+
+        ZStack {
+
+            LinearGradient(colors: [.orange, .blue],
+                           startPoint: .top,
+                           endPoint: .bottom)
+            .ignoresSafeArea()
+
+            HStack {
+                Button {
+                    print("Go to happy list")
+                } label: {
+                    FeelingsButtonsView(iconFeeling: "☺️",
+                                        title: "Happy",
+                                        backgroundButtonColor: Color.green)
+                }
+
+                Button {
+                    print("Go to normal list")
+                } label: {
+                    FeelingsButtonsView(iconFeeling: "😐",
+                                        title: "Normal",
+                                        backgroundButtonColor: Color.blue)
+                }
+
+                Button {
+                    print("Go to sad list")
+                } label: {
+                    FeelingsButtonsView(iconFeeling: "😢",
+                                        title: "Triste",
+                                        backgroundButtonColor: Color.red)
+                }
             }
         }
     }
